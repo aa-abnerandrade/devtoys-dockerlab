dd4ca4d7c18f52d8c96203d1fa7c2685e13b2112
abnerandrade <aa.abnerandrade@outlook.com.br>
2026-04-24 01:16:04 -0300

    chore(init): initialize Spring Boot application with Docker support and Maven wrapper

---
+++ src/test/java/com/devtoys/dockerlab/DockerlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtoys.dockerlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
